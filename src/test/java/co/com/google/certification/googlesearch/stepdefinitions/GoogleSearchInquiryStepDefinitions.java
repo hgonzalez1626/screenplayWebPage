bfcbfba2071907420082526667bbb91a63a47376
hgonzalez1626 <hgonzalez1626@gmail.com>
2022-04-12 19:57:56 -0500

    Se agrega el arquetipo de screenplay y se construye el feature, el step definitions y el runner

---
+++ src/test/java/co/com/google/certification/googlesearch/stepdefinitions/GoogleSearchInquiryStepDefinitions.java
@@ -0,0 +1,22 @@
+package co.com.google.certification.googlesearch.stepdefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class GoogleSearchInquiryStepDefinitions {
+    @Given("^User visit the google web search$")
+    public void userVisitTheGoogleWebSearch() throws Exception {
+
+    }
+
+    @When("^User trie to search the next information$")
+    public void userTrieToSearchTheNextInformation(String arg1) throws Exception {
+
+    }
+
+    @Then("^User verifies is success for the search$")
+    public void userVerifiesIsSuccessForTheSearch() throws Exception {
+        
+    }
+}
